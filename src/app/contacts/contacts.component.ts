c723dd9253fdca1c103dbfc27f9974d5abd563ba
Diogo Azevedo <diogoazevedo541@gmail.com>
2025-08-29 20:39:46 +0100

    added a toast from angular material

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
-import { LucideAngularModule, Mail } from 'lucide-angular';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { LucideAngularModule, Mail, Copy } from 'lucide-angular';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-contacts',
@@ -10,4 +12,29 @@ import { LucideAngularModule, Mail } from 'lucide-angular';
 })
 export class ContactsComponent {
   readonly mail = Mail;
+  readonly copy = Copy;
+
+  private _snackBar = inject(MatSnackBar);
+
+  constructor(
+    private translate: TranslateService,
+    private clipboard: Clipboard
+  ) {}
+
+  durationInSeconds = 3;
+
+  openSnackBar(message: string) {
+    const fullMessage = this.translate.instant(message);
+    if (fullMessage) {
+      this.clipboard.copy(fullMessage);
+      this._snackBar.open(
+        this.translate.instant('contact.snackbar.copy'),
+        this.translate.instant('contact.snackbar.close'),
+        {
+          duration: this.durationInSeconds * 1000,
+          panelClass: ['snackbar-success'],
+        }
+      );
+    }
+  }
 }
